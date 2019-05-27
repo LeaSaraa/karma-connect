37bb2641d0b7addd9ad2349bfbdeb0559d73da2b
Omolola Fagade <omololafagade@gmail.com>
2019-05-27 15:02:03 +0200

    Neighbourhood, user and posts model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_100829) do
+ActiveRecord::Schema.define(version: 2019_05_27_125832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "neighbourhoods", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "address"
+    t.string "title"
+    t.string "description"
+    t.string "picture"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
+    t.integer "karma_points"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email", default: "", null: false
+    t.string "upload_avatar"
+    t.string "personal_info"
+    t.bigint "neighbourhood_id"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -24,7 +47,10 @@ ActiveRecord::Schema.define(version: 2019_05_27_100829) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["neighbourhood_id"], name: "index_users_on_neighbourhood_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
+  add_foreign_key "users", "neighbourhoods"
 end
