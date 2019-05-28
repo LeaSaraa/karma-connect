1a59449ebcd674b3aa5412d59c0bc7716ef0467d
Omolola Fagade <omololafagade@gmail.com>
2019-05-28 11:49:01 +0200

    Search bar works by user first name or last name or by post title

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
   def index
+    if params[:query].present?
+      sql_query = " \
+        posts.title ILIKE :query \
+        OR users.first_name ILIKE :query \
+        OR users.last_name ILIKE :query \
+      "
+      @posts = Post.joins(:user).where(sql_query, query: "%#{params[:query]}%")
+    else
     @posts = Post.all
+    end
   end
 end
