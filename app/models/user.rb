c46cdf2f7c6be0193b29b673af4af8a3e88ff920
Lea Agterberg <lea.sara.agterberg@gmail.com>
2019-05-29 12:46:32 +0200

    Added the User controller to create the User profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :upload_avatar, PhotoUploader
+
+  validates :first_name, :last_name, :email, :upload_avatar, :personal_info, presence: true
 end
