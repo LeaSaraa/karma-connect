838889dd3940ae2fada1c1650588b67f0cf04ea3
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-05-30 00:08:31 +0200

    styling of post page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :show]
+
   resources :my_posts , only: [:new, :create, :index]
   resources :users, only: [:show, :update, :edit]
+
 end
