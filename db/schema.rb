e8092642c17ed5e29ba787839a1e1bb6fc81b605
Lea Agterberg <lea.sara.agterberg@gmail.com>
2019-05-31 14:56:38 +0200

    REDISTOGO_URL

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_05_30_181445) do
     t.datetime "updated_at", null: false
     t.integer "sender_id"
     t.integer "receiver_id"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
