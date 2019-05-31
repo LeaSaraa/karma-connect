9086fb487ee3c0a1ac88b60d804a435c6f77e621
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-05-31 15:35:07 +0200

    comment styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_181445) do
+ActiveRecord::Schema.define(version: 2019_05_31_111339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,9 @@ ActiveRecord::Schema.define(version: 2019_05_30_181445) do
     t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "compliments", force: :cascade do |t|
@@ -75,6 +77,7 @@ ActiveRecord::Schema.define(version: 2019_05_30_181445) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "compliments", "posts"
   add_foreign_key "compliments", "users"
   add_foreign_key "posts", "users"
