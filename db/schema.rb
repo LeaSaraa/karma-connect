89def598cbe9218feefa91c0b26a638ffde8e075
Lea Agterberg <lea.sara.agterberg@gmail.com>
2019-06-03 17:33:44 +0200

    Last updates on Seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_06_03_111930) do
 
   # These are extensions that must be enabled in order to support this database
@@ -78,6 +77,7 @@ ActiveRecord::Schema.define(version: 2019_06_03_111930) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "category"
     t.string "status", default: "Active"
     t.bigint "category_id"
     t.index ["category_id"], name: "index_posts_on_category_id"
