ba89d8977a2541b7a781b505d26168eb14229a24
Lea Agterberg <lea.sara.agterberg@gmail.com>
2019-06-06 12:35:29 +0200

    change minor things about chat formc

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -12,7 +12,8 @@ App.messages = App.cable.subscriptions.create('MessagesChannel', {
 
     received: function (data) {
       // Called when there's incoming data on the websocket for this channel. You could put plain jQuery into here to replace a div with html, for example.
-      document.querySelector('#messages').classList.remove('hidden')
+      document.querySelector('#messages').classList.remove('hidden');
+      document.querySelector('#message_content').value = "";
       var content = "<div class='message-container'><div class='avatar-wrapper-left'><img class='message-avatar' src='" + data.url + "'><p>" + data.user + "</p></div><div class='message-text-balloon-left'>" + data.message + "</div></div>"
       return document.querySelector('#messages').insertAdjacentHTML('beforeend', content);
     }
